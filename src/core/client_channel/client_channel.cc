50097a54ecf72b8637eee3e335c7e3848c775d42
Akshit Patel <akshitpatel@google.com>
2026-07-07 23:49:14 -0700

    [export] [CallV3] Apply channel args mutator for V3 channel
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/944305318](http://cl/944305318)
    
    PiperOrigin-RevId: 944305318

--- src/core/client_channel/client_channel.cc
+++ src/core/client_channel/client_channel.cc
@@ -237,10 +237,12 @@ class ClientChannel::SubchannelWrapper::WatcherWrapper
         << subchannel_wrapper_.get() << "; hopping into work_serializer";
     auto self = RefAsSubclass<WatcherWrapper>();
     subchannel_wrapper_->client_channel_->work_serializer_->Run(
-        [self, state, status]() ABSL_EXCLUSIVE_LOCKS_REQUIRED(
-            *self->subchannel_wrapper_->client_channel_->work_serializer_) {
-          self->ApplyUpdateInControlPlaneWorkSerializer(state, status);
-        });
+        [self, state, status]()
+            ABSL_EXCLUSIVE_LOCKS_REQUIRED(*self->subchannel_wrapper_
+                                              ->client_channel_
+                                              ->work_serializer_) {
+              self->ApplyUpdateInControlPlaneWorkSerializer(state, status);
+            });
   }
 
   void OnKeepaliveUpdate(Duration new_keepalive_time) override {
@@ -250,10 +252,13 @@ class ClientChannel::SubchannelWrapper::WatcherWrapper
         << subchannel_wrapper_.get() << "; hopping into work_serializer";
     auto self = RefAsSubclass<WatcherWrapper>();
     subchannel_wrapper_->client_channel_->work_serializer_->Run(
-        [self, new_keepalive_time]() ABSL_EXCLUSIVE_LOCKS_REQUIRED(
-            *self->subchannel_wrapper_->client_channel_->work_serializer_) {
-          self->ApplyKeepaliveThrottlingInWorkSerializer(new_keepalive_time);
-        });
+        [self, new_keepalive_time]()
+            ABSL_EXCLUSIVE_LOCKS_REQUIRED(*self->subchannel_wrapper_
+                                              ->client_channel_
+                                              ->work_serializer_) {
+              self->ApplyKeepaliveThrottlingInWorkSerializer(
+                  new_keepalive_time);
+            });
   }
 
   uint32_t max_connections_per_subchannel() const override {
@@ -387,8 +392,8 @@ void ClientChannel::SubchannelWrapper::Orphaned() {
   auto self = WeakRefAsSubclass<SubchannelWrapper>(DEBUG_LOCATION,
                                                    "subchannel map cleanup");
   client_channel_->work_serializer_->Run(
-      [self]() ABSL_EXCLUSIVE_LOCKS_REQUIRED(
-          *self->client_channel_->work_serializer_) {
+      [self]() ABSL_EXCLUSIVE_LOCKS_REQUIRED(*self->client_channel_
+                                                 ->work_serializer_) {
         auto it = self->client_channel_->subchannel_map_.find(
             self->subchannel_.get());
         GRPC_CHECK(it != self->client_channel_->subchannel_map_.end());
@@ -585,6 +590,12 @@ absl::StatusOr<RefCountedPtr<Channel>> ClientChannel::Create(
   // Get URI to resolve, using proxy mapper if needed.
   if (target.empty()) {
     return absl::InternalError("target URI is empty in client channel");
+  }
+  auto channel_args_mutator =
+      grpc_channel_args_get_client_channel_creation_mutator();
+  if (channel_args_mutator != nullptr) {
+    channel_args =
+        channel_args_mutator(target.c_str(), channel_args, GRPC_CLIENT_CHANNEL);
   }
   std::string uri_to_resolve = CoreConfiguration::Get()
                                    .proxy_mapper_registry()
